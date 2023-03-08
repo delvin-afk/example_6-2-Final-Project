78d72497765c84187304ae23c3ef271f13098414
xemcat <93665141+xemcat@users.noreply.github.com>
2023-03-08 12:23:04 -0500

    Update servo_door.cpp

--- modules/servo_door/servo_door.cpp
+++ modules/servo_door/servo_door.cpp
@@ -14,17 +14,20 @@ PwmOut servo(PF_9);
 DigitalOut testLED(LED2);   //can be used in the code below to test if code runs in this module
 
 static void closeDoor();    //changed from lowerGate --> does nothing tbh
+bool isOpenDoor = false;
 
 //servo_doorInit: changed from the servo_gateInit. Closes the door (move to original position)
 void servo_doorInit()        
 {
     servo.period(PERIOD);
     servo.write(DUTY_MIN);
+    isOpenDoor = false;
 }
 
 //opens door for 10 seconds; the function that calls openDoor should also call servo_doorInit to close the door 
 void openDoor() //changed from raiseGate
 {
     servo.write(DUTY_MAX);  
-    delay(10000);           
+    delay(10000);
+    isOpenDoor = true;
 }
